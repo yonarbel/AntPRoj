c2c48e5fd8eb29172cfae8549f0343a368351a19
yonatana <yonatana@jfrog.com>
2021-10-11 16:17:35 +0300

    re ordering ci and files

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueNativeSock from 'vue-native-websocket'
 Vue.config.productionTip = false
-
-Vue.use(VueNativeSock, 'ws://localhost:3000', {
+const host = location.host.split(':')[0]
+Vue.use(VueNativeSock, `ws://${host}:3000`, {
   reconnection: true, // (Boolean) whether to reconnect automatically (false)
   reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
   reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
